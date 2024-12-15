280d36b5118158971b0f26b3b6b7ecc28f024fe6
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-15 23:33:27 +0800

    Initial setup and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#f9fafb',
+      'black': '#030712',
+      'gray': '#6b7280',
+      'dark-gray': '#1f2937',
+      'light-gray': '#9ca3af',
+      'red': '#ef4444',
+      'rose': '#f43f5e',
+      'blue': '#3b82f6',
+    },
+    extend: {
+    
+    },
+  },
+  plugins: [],
+}
+
