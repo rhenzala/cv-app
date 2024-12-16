b91cef6b2dfbd7bad8ca2acebf846b0975eaa79e
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-16 09:59:24 +0800

    Add Education and other details to PersonalDetails
    Add the contact details such as city address, phone number, and email address.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,43 @@ import Preview from './Preview'
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [cityAddress, setCityAddress] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+  const [email, setEmail] = useState('');
+  const [schoolName, setSchoolName] = useState('');
+  const [fieldStudy, setFieldStudy] = useState('');
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
+
   
   function handleFirstNameChange(e) {
-      setFirstName(e.target.value);
+    setFirstName(e.target.value);
   }
-  
   function handleLastNameChange(e) {
-      setLastName(e.target.value);
+    setLastName(e.target.value);
   }
+  function handleCityChange(e) {
+    setCityAddress(e.target.value);
+  }
+  function handlePhoneChange(e) {
+    setPhoneNum(e.target.value);
+  }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+  function handleSchoolNameChange(e) {
+    setSchoolName(e.target.value);
+  }
+  function handleFieldStudyChange(e) {
+    setFieldStudy(e.target.value);
+  }
+  function handleFromChange(e) {
+    setFromDate(e.target.value);
+  }
+  function handleToChange(e) {
+    setToDate(e.target.value);
+  }
+
   return (
     <>
       <div className='flex w-full h-full'>
@@ -22,10 +51,31 @@ function App() {
         lastName={lastName} 
         handleFirstNameChange={handleFirstNameChange}
         handleLastNameChange={handleLastNameChange}
+        cityAddress={cityAddress}
+        phoneNum={phoneNum}
+        email={email}
+        handleCityChange={handleCityChange}
+        handlePhoneChange={handlePhoneChange}
+        handleEmailChange={handleEmailChange}
+        schoolName={schoolName}
+          handleSchoolNameChange={handleSchoolNameChange}
+          fieldStudy={fieldStudy}
+          handleFieldStudyChange={handleFieldStudyChange}
+          fromDate={fromDate}
+          handleFromChange={handleFromChange}
+          toDate={toDate}
+          handleToChange={handleToChange} 
         />
         <Preview 
         firstName={firstName}
         lastName={lastName}
+        cityAddress={cityAddress}
+        phoneNum={phoneNum}
+        email={email}
+        schoolName={schoolName}
+        fieldStudy={fieldStudy}
+        fromDate={fromDate}
+        toDate={toDate} 
         />
       </div>
     </>
