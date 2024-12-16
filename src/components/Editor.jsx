96025c81b62627866831ad90ce69e6a5f1d5b1aa
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-16 10:05:37 +0800

    Add Education and contact details

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,51 +1,50 @@
+import PersonalDetails from "./PersonalDetails"
+import Education from "./Education"
 
 
-
-function PersonalForm({
-    firstName, 
-    lastName, 
-    handleFirstNameChange,
-    handleLastNameChange
-}) { 
-    return (
-        <section className="bg-light-gray p-8 rounded-md">
-            <div className="flex justify-between mb-4">
-                <h1 className="font-semibold text-xl">Personal Details</h1>
-                <button>Show</button>
-            </div>
-            <form action="">
-                <div>
-                    <label htmlFor="firstName">First Name</label>
-                    <input 
-                    type="text"
-                    id="firstName"
-                    value={firstName}
-                    onChange={handleFirstNameChange}
-                     />
-                </div>
-                <div>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input 
-                    type="text"
-                    id="lastName"
-                    value={lastName}
-                    onChange={handleLastNameChange}
-                     />
-                </div>
-            </form>
-        </section>
-         
-    )
-}
-
-export default function Editor ({firstName, lastName, handleFirstNameChange, handleLastNameChange}) {
+export default function Editor ({
+        firstName, 
+        lastName, 
+        handleFirstNameChange, 
+        handleLastNameChange,
+        cityAddress,
+        phoneNum,
+        email,
+        handleCityChange,
+        handlePhoneChange,
+        handleEmailChange,
+        schoolName, 
+        handleSchoolNameChange,
+        fieldStudy, 
+        handleFieldStudyChange,
+        fromDate,
+        handleFromChange,
+        toDate,
+        handleToChange
+    }) {
     return (
         <div className="editor p-8 text-black w-[40%] flex flex-col gap-3">
-            <PersonalForm
+            <PersonalDetails
             firstName={firstName} 
             lastName={lastName}
             handleFirstNameChange={handleFirstNameChange}
             handleLastNameChange={handleLastNameChange}
+            cityAddress={cityAddress}
+            phoneNum={phoneNum}
+            email={email}
+            handleCityChange={handleCityChange}
+            handlePhoneChange={handlePhoneChange}
+            handleEmailChange={handleEmailChange}
+            />
+            <Education
+            schoolName={schoolName}
+            handleSchoolNameChange={handleSchoolNameChange}
+            fieldStudy={fieldStudy}
+            handleFieldStudyChange={handleFieldStudyChange}
+            fromDate={fromDate}
+            handleFromChange={handleFromChange}
+            toDate={toDate}
+            handleToChange={handleToChange} 
             />
         </div>
     )
