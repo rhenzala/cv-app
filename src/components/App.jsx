e50299e6ca4a3c4a2736ee770987a65e610fa721
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-17 15:03:53 +0800

    Rework Education and Experience components
    They will now require fewer parameters for better code readability, also put the inputs inside an array.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,137 @@ import '../styles/App.css'
 import Editor from './Editor'
 import Preview from './Preview'
 
+
 function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [cityAddress, setCityAddress] = useState('');
-  const [phoneNum, setPhoneNum] = useState('');
-  const [email, setEmail] = useState('');
-  const [schoolName, setSchoolName] = useState('');
-  const [fieldStudy, setFieldStudy] = useState('');
-  const [fromDate, setFromDate] = useState('');
-  const [toDate, setToDate] = useState('');
+  const [firstName, setFirstName] = useState('John');
+  const [lastName, setLastName] = useState('Doe');
+  const [cityAddress, setCityAddress] = useState('London, UK');
+  const [phoneNum, setPhoneNum] = useState('123456789');
+  const [email, setEmail] = useState('johndoe@email.com');
+
+  const [education, setEducation] = useState([
+    {
+      id: 1,
+      school: 'Somewhere College',
+      fieldStudy: 'BS Computer Science',
+      fromDate: '2014-08',
+      toDate: '2018-09',
+      descriptions: []
+    }
+  ])
+  const [experiences, setExperiences] = useState([
+    {
+      id: 1,
+      company: 'This Company, Inc.',
+      role: 'Software Engineer',
+      fromWorkDate: '2018-03',
+      toWorkDate: '2022-09',
+      descriptions: []
+    }
+  ])
+  const updateArray = (sec, id, field, value) => {
+    if (sec === 'Experience') {
+      setExperiences(prevExperiences => 
+        prevExperiences.map(exp => 
+          exp.id === id ? {...exp, [field]:value}: exp
+        ))
+    }
+    if (sec === 'Education') {
+      setEducation(prevEducation => 
+        prevEducation.map(edu => 
+          edu.id === id ? {...edu, [field]:value}: edu
+        ))
+    }
+    
+  }
+  const addDescription = (sec, experienceId, description) => {
+    if (sec === 'Experience') {
+      setExperiences(prevExperiences => 
+        prevExperiences.map(exp => 
+          exp.id === experienceId ? {...exp, descriptions: [...exp.descriptions, description]}: exp
+        ));
+    }
+    if (sec === 'Education') {
+      setEducation(prevEducation => 
+        prevEducation.map(edu => 
+          edu.id === experienceId ? {...edu, descriptions: [...edu.descriptions, description]}: edu
+        ));
+    }
+    
+  }
+  const deleteDescription = (sec, experienceId, descriptionIndex) => {
+    if (sec === 'Experience') {
+      setExperiences(prevExperiences => 
+        prevExperiences.map(exp => 
+          exp.id === experienceId ? 
+          {
+            ...exp, 
+            descriptions: exp.descriptions.filter((_, i) => i !== descriptionIndex)
+          } 
+          : exp
+        ))
+    }
+    if (sec === 'Education') {
+      setEducation(prevEducation => 
+        prevEducation.map(edu => 
+          edu.id === experienceId ? 
+          {
+            ...edu, 
+            descriptions: edu.descriptions.filter((_, i) => i !== descriptionIndex)
+          } 
+          : edu
+        ))
+    }
+    
+  }
+  const addForm = (sec) => {
+    if (sec === 'Experience') {
+      // Math.max() is used to avoid duplication of id when deleting and then adding experience
+      const newId = experiences.length > 0 ?
+      Math.max(...experiences.map(exp => exp.id), 0) + 1
+      : 1;
+      const newExperience = {
+        id: newId,
+        company: '',
+        role: '',
+        fromWorkDate: '',
+        toWorkDate: '',
+        descriptions: []
+      };
+
+      setExperiences(prevExperiences => [...prevExperiences, newExperience]);
+    }
+    if (sec === 'Education') {
+      // Math.max() is used to avoid duplication of id when deleting and then adding experience
+      const newId = education.length > 0 ?
+      Math.max(...education.map(edu => edu.id), 0) + 1
+      : 1;
+      const newEducation = {
+        id: newId,
+        school: '',
+        fieldStudy: '',
+        fromDate: '',
+        toDate: '',
+        descriptions: []
+      };
+
+      setEducation(prevEducation => [...prevEducation, newEducation]);
+    }
+    
+  }
+  const deleteForm = (sec, id) => {
+    if (sec === 'Experience') {
+      setExperiences(prevExperiences => 
+        prevExperiences.filter(exp => exp.id !== id)
+      )
+    }
+    if (sec === 'Education') {
+      setEducation(prevEducation => 
+        prevEducation.filter(edu => edu.id !== id)
+      )
+    }
+  }
+
 
   
   function handleFirstNameChange(e) {
@@ -30,18 +151,7 @@ function App() {
   function handleEmailChange(e) {
     setEmail(e.target.value);
   }
-  function handleSchoolNameChange(e) {
-    setSchoolName(e.target.value);
-  }
-  function handleFieldStudyChange(e) {
-    setFieldStudy(e.target.value);
-  }
-  function handleFromChange(e) {
-    setFromDate(e.target.value);
-  }
-  function handleToChange(e) {
-    setToDate(e.target.value);
-  }
+  
 
   return (
     <>
@@ -57,14 +167,13 @@ function App() {
         handleCityChange={handleCityChange}
         handlePhoneChange={handlePhoneChange}
         handleEmailChange={handleEmailChange}
-        schoolName={schoolName}
-          handleSchoolNameChange={handleSchoolNameChange}
-          fieldStudy={fieldStudy}
-          handleFieldStudyChange={handleFieldStudyChange}
-          fromDate={fromDate}
-          handleFromChange={handleFromChange}
-          toDate={toDate}
-          handleToChange={handleToChange} 
+        education={education}
+        experiences={experiences}
+        updateArray={updateArray}
+        addDescription={addDescription}
+        deleteDescription={deleteDescription}
+        addForm={addForm}
+        deleteForm={deleteForm}
         />
         <Preview 
         firstName={firstName}
@@ -72,10 +181,8 @@ function App() {
         cityAddress={cityAddress}
         phoneNum={phoneNum}
         email={email}
-        schoolName={schoolName}
-        fieldStudy={fieldStudy}
-        fromDate={fromDate}
-        toDate={toDate} 
+        education={education}
+        experiences={experiences}
         />
       </div>
     </>
