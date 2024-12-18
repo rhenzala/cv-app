2d6ac3825cff5378253d997e303575f8cb9393dc
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-18 13:51:10 +0800

    Install and configure prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#f9fafb',
-      'black': '#030712',
-      'gray': '#6b7280',
-      'dark-gray': '#1f2937',
-      'light-gray': '#e5e7eb',
-      'red': '#ef4444',
-      'rose': '#f43f5e',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#f9fafb",
+      black: "#030712",
+      gray: "#6b7280",
+      "dark-gray": "#1f2937",
+      "light-gray": "#e5e7eb",
+      red: "#ef4444",
+      rose: "#f43f5e",
     },
     extend: {
       screens: {
-        'custom-sm': { max: '680px' }, 
-        'custom-md': { max: '1100px' }
+        "custom-sm": { max: "680px" },
+        "custom-md": { max: "1100px" },
       },
     },
   },
   plugins: [],
-}
-
+};
